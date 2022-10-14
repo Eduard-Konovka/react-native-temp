78553d42d954e8a18b693b165dfb51a8b88eeab6
Eduard Konovka <ed098ua@gmail.com>
2022-10-14 14:13:50 +0300

    Main Components (View, Text, Button, Alert, Image)

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, Alert } from 'react-native';
+import {
+  StyleSheet,
+  Button,
+  Text,
+  View,
+  Alert,
+  Image,
+  TouchableWithoutFeedback,
+} from 'react-native';
+
+const Separator = () => <View style={styles.separator} />;
 
 export default function App() {
-  const buttonClick = () => Alert.alert('Button pressed');
+  const buttonClick = () =>
+    Alert.alert('Героям слава!', 'Выбирите действие', [
+      {
+        text: 'Yes',
+        onPress: () => console.log('Clicks Yes button'),
+      },
+      {
+        text: 'No',
+        onPress: () => console.log('Clicks No button'),
+      },
+    ]);
+  const buttonClick2 = () =>
+    Alert.alert('Смерть ворогам!', 'Выбирите действие', [
+      {
+        text: 'Yes',
+        onPress: () => console.log('Clicks Yes button'),
+      },
+      {
+        text: 'No',
+        onPress: () => console.log('Clicks No button'),
+      },
+    ]);
   const textPress = () => Alert.alert('Text pressed');
 
   return (
     <View style={styles.container}>
       <Text style={styles.text} onPress={textPress}>
-        Hello, World!
+        Hello, world!
       </Text>
       <Button
         onPress={buttonClick}
-        title="Click me!"
+        title="Слава Україні!"
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
       />
+      <Separator />
+      <Button
+        onPress={buttonClick2}
+        title="Слава Нації!"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <Separator />
+      <TouchableWithoutFeedback onPress={buttonClick}>
+        <Image
+          source={{
+            uri: 'https://avatars.githubusercontent.com/u/80203865?s=400&u=d0e491aecc78f03372cead8c427045ed614c92c1&v=4',
+            width: 200,
+            height: 200,
+          }}
+        />
+      </TouchableWithoutFeedback>
+      <Separator />
+      <Image
+        source={require('./assets/react.png')}
+        style={{ width: 230, height: 200 }}
+        blurRadius={2}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -30,5 +85,10 @@ const styles = StyleSheet.create({
   },
   text: {
     margin: 20,
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
