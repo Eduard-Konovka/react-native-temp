6d9e5b9c1f65e12481700da38d4d737767a13f23
Eduard Konovka <ed098ua@gmail.com>
2022-10-20 23:56:15 +0300

    Connects styles

--- chapters/Chapter6.jsx
+++ chapters/Chapter6.jsx
@@ -1,12 +1,23 @@
-import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { View, AppLoading } from 'react-native';
+import { globalStyles } from '../styles/style';
+import * as Font from 'expo-font';
+
+const fonts = () =>
+  Font.loadAsync({
+    bold: require('../assets/fonts/Montserrat-Bold.ttf'),
+    light: require('../assets/fonts/Montserrat-Light.ttf'),
+  });
 
 export default function Chapter6() {
-  return <View style={styles.main}></View>;
-}
+  const [font, setFont] = useState(false);
 
-const styles = StyleSheet.create({
-  main: { flex: 1 },
-});
+  return (
+    <View>
+      {font ? <View style={globalStyles.main}></View> : <AppLoading />}
+    </View>
+  );
+}
 
 Chapter6.defaultProps = {
   children: null,
